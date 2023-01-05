a49b8548f08be23bb0bc591a4b8a94b406c7e47e
5237-mests <msfnmulgeta@gmail.com>
2023-01-05 15:10:46 +0300

    flor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module having floor function.
+"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Function used for converting float to integer."""
+    return math.floor(n)
