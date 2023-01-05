e114021c69af0c3a548123efd18db058e8cd4d03
5237-mests <99667573+5237-mests@users.noreply.github.com>
2023-01-05 11:30:19 +0300

    add float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module having add function.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Function adding two float number and return sum as float type."""
+    return a + b
