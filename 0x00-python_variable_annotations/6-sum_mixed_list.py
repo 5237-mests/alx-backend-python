9b76e6069637a2a7b4335b4c6dba09f7eadfd089
5237-mests <msfnmulgeta@gmail.com>
2023-01-05 16:36:02 +0300

    doc

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Module contain sum_mixed function."""
+
 from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """Function takes mixed list and returns sum as floating num."""
     return float(sum(mxd_lst))
