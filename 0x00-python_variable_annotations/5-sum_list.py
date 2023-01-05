2a41ac0ea85331ca1879dcb81ea167c96441b133
5237-mests <msfnmulgeta@gmail.com>
2023-01-05 16:11:40 +0300

    complexlist

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contain function"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Sum_list returns sum of list as float"""
+    return sum(input_list)
