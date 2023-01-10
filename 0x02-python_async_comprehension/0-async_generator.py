60ef262b611849e030ee27c41f88252274820080
5237-mests <msfnmulgeta@gmail.com>
2023-01-10 12:29:02 +0300

    type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 """
 import random
 import asyncio
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """Asyncronous Generator function"""
     for i in range(10):
         await asyncio.sleep(1)
