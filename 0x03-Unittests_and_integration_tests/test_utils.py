51f34368e22677c429a22871974f19642b7a3bdc
5237-mests <msfnmulgeta@gmail.com>
2023-01-28 23:43:02 +0300

    mock

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """Module
 """
 import unittest
+from unittest.mock import patch, Mock
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
 
@@ -27,7 +28,7 @@ class TestAccessNestedMap(unittest.TestCase):
                                ) -> None:
         """test function"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
-    
+
     @parameterized.expand([
         ({}, ("a", ), KeyError),
         ({"a": 1}, ("a", "b"), KeyError),
@@ -40,3 +41,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """raises exception"""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """class
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """test get_json function"""
+        attrs = {'json.return_value': test_payload}
+        with patch("requests.get", return_value=Mock(**attrs)) as req_get:
+            self.assertEqual(get_json(test_url), test_payload)
+            req_get.assert_called_once_with(test_url)
